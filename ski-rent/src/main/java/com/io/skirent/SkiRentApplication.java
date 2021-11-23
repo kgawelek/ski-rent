bc70bd418217a95908ce9d51701be1e7ffd3b51a
I552641 <konrad.gawelek@sap.com>
2021-11-23 19:37:42 +0100

    spring project init

---
+++ ski-rent/src/main/java/com/io/skirent/SkiRentApplication.java
@@ -0,0 +1,13 @@
+package com.io.skirent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkiRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkiRentApplication.class, args);
+	}
+
+}
---
+++ ski-rent/src/test/java/com/io/skirent/SkiRentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.skirent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkiRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
