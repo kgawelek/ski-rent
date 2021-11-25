4a7830ca713c7364fe0a3b59c0bc98542fe60a2d
Piotr Rajnisz <rajniszp@gmail.com>
2021-11-25 23:46:00 +0100

    Przebudowa usera na account i user dla zgodności z bazą danych, dodanie AccountManagera

--- ski-rent/src/main/java/com/io/skirent/user/User.java
+++ ski-rent/src/main/java/com/io/skirent/user/User.java
@@ -4,32 +4,8 @@ package com.io.skirent.user;
 import javax.persistence.*;
 import java.time.LocalDate;
 
-@Entity
-@Table(
-        name = "users",
-        uniqueConstraints = {
-                @UniqueConstraint(name = "user_email", columnNames = "email"),
-                @UniqueConstraint(name = "user_login", columnNames = "login")
-        }
-)
-public class User {
-
-    @Id
-    @SequenceGenerator(
-            name = "user_sequence",
-            sequenceName = "user_sequence",
-            allocationSize = 1
-    )
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "user_sequence"
-    )
-    @Column(
-            name = "id",
-            nullable = false
-    )
-    private Long id;
-
+@MappedSuperclass
+abstract class User extends Account {
     @Column(
             name = "name",
             nullable = false,
@@ -38,46 +14,11 @@ public class User {
     private String name;
 
     @Column(
-            name = "login",
-            nullable = false,
-            columnDefinition = "TEXT"
-    )
-    private String login;
-
-    @Column(
             name = "birthday",
             columnDefinition = "DATE"
 
     )
     private LocalDate dob;
-
-    @Column(
-            name = "email",
-            nullable = false,
-            unique = true,
-            columnDefinition = "TEXT"
-    )
-    private String email;
-
-    public User() {
-
-    }
-
-    public User(Long id, String name, String login, LocalDate dob, String email) {
-        this.id = id;
-        this.name = name;
-        this.login = login;
-        this.dob = dob;
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
@@ -87,27 +28,11 @@ public class User {
         this.name = name;
     }
 
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
     public LocalDate getDob() {
         return dob;
     }
 
     public void setDob(LocalDate dob) {
         this.dob = dob;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 }
