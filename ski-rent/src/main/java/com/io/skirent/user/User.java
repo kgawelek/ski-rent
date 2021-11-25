c842489fb46b8c0b62ff9194633f9b1444dfaba4
I552641 <konrad.gawelek@sap.com>
2021-11-25 18:41:16 +0100

    fixing sequence name

--- ski-rent/src/main/java/com/io/skirent/user/User.java
+++ ski-rent/src/main/java/com/io/skirent/user/User.java
@@ -16,13 +16,13 @@ public class User {
 
     @Id
     @SequenceGenerator(
-            name = "student_sequence",
-            sequenceName = "student_sequence",
+            name = "user_sequence",
+            sequenceName = "user_sequence",
             allocationSize = 1
     )
     @GeneratedValue(
             strategy = GenerationType.SEQUENCE,
-            generator = "student_sequence"
+            generator = "user_sequence"
     )
     @Column(
             name = "id",
