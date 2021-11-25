9999126769ffe6c2e64ac4cd2f5e70bbec585775
I552641 <konrad.gawelek@sap.com>
2021-11-25 18:40:15 +0100

    added equipment class and category enum

---
+++ ski-rent/src/main/java/com/io/skirent/equipment/Category.java
@@ -0,0 +1,16 @@
+package com.io.skirent.equipment;
+
+public enum Category {
+    SKI,
+    SNOWBOARD,
+    SKI_POLES,
+    SKI_BOOTS,
+    SNOWBOARD_BOOTS,
+    HELMET,
+    JACKET,
+    PANTS,
+    CAP,
+    SCARF,
+    GLOVES,
+    OTHER;
+}
---
+++ ski-rent/src/main/java/com/io/skirent/equipment/Equipment.java
@@ -0,0 +1,113 @@
+package com.io.skirent.equipment;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(
+        name = "equipments"
+)
+public class Equipment {
+
+    @Id
+    @SequenceGenerator(
+            name = "equipment_sequence",
+            sequenceName = "equipment_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "equipment_sequence"
+    )
+    @Column(
+            name = "id",
+            nullable = false
+    )
+    private Long id;
+
+    @Column(
+            name = "name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String name;
+
+    @Column(
+            name = "manufacturer",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String manufacturer;
+
+    @Column(
+            name = "size",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String size;
+
+    @Column(
+            name = "next_check_up",
+            columnDefinition = "DATE"
+
+    )
+    private LocalDate nextCheckUp;
+
+    @Column(
+            name = "category",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private Category category;
+
+    public Equipment() {
+    }
+
+    public Equipment(String name, String manufacturer, String size, LocalDate nextCheckUp, Category category) {
+        this.name = name;
+        this.manufacturer = manufacturer;
+        this.size = size;
+        this.nextCheckUp = nextCheckUp;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public LocalDate getNextCheckUp() {
+        return nextCheckUp;
+    }
+
+    public void setNextCheckUp(LocalDate nextCheckUp) {
+        this.nextCheckUp = nextCheckUp;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
