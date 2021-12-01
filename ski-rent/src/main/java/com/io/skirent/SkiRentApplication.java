17f93622fceacfa3154b8ed4e8c57d73b9b790e8
kgawelek <konradgawelek@gmail.com>
2021-12-01 11:15:34 +0100

    moved frontend files so it is automatically connected to spring app

--- ski-rent/src/main/java/com/io/skirent/SkiRentApplication.java
+++ ski-rent/src/main/java/com/io/skirent/SkiRentApplication.java
@@ -3,11 +3,12 @@ package com.io.skirent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDate;
+
 @SpringBootApplication
 public class SkiRentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SkiRentApplication.class, args);
 	}
-
 }
