4e876c57d4b35a3a0c3f5c521669cab5ca90751b
Piotr Rajnisz <rajniszp@gmail.com>
2021-12-13 23:09:31 +0100

    Spring security - wygląda na to, że działa z bazą danych; punkt przywracania

--- ski-rent/src/main/java/com/io/skirent/SecurityConfiguration.java
+++ ski-rent/src/main/java/com/io/skirent/SecurityConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -18,56 +18,75 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     DataSource dataSource;
 
+    // URIs:
     private static final String adminUri = "/admin/**";
     private static final String employeeUri = "/employee/**";
     private static final String clientUri = "/client/**";
     private static final String apiUserUri = "/api/user/**";
+    private static final String registerUri = "/api/user/register";
 
+    // roles:
+    private static final String rolePrefix = "ROLE_";
     private static final String adminRole = "ADMIN";
     private static final String employeeRole = "EMPLOYEE";
     private static final String clientRole = "CLIENT";
     private static final String userRole = "USER";
 
+    // login page data:
     private static final String loginPage = "/login.jsp";
     private static final String loginFailurePostfix = "?error=1";
     private static final String loginProcessingUrl = "/login";
     private static final String logoutSuccessfulUrl = "/index.html";
 
+    /**
+     * Accounts configuration: setting users and their roles, pulling data from database.
+     * @param auth security builder
+     * @throws Exception when jdbcAuthentication() fails
+     */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        //PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 
-        // TODO: change to database ↓
-//        auth.inMemoryAuthentication()
-//                .withUser("user")
-//                .password(encoder.encode("password"))
-//                .roles("USER")
-//                .and()
-//                .withUser("admin")
-//                .password(encoder.encode("admin"))
-//                .roles("ADMIN");
+        String authoritiesQuery = """
+                select email,'%s' as authority
+                from accounts ac join "admin" ad on ac.user_id = ad.id
+                union
+                select email,'%s' as authority
+                from accounts ac join employee em on ac.user_id = em.id
+                union
+                select email,'%s' as authority
+                from accounts ac join client cl on ac.user_id = cl.id
+                where email = ?
+                """
+                .formatted(rolePrefix + adminRole + ", " + rolePrefix + employeeRole + ", " + rolePrefix + userRole, // admin roles
+                    rolePrefix + employeeRole + ", " + rolePrefix + userRole, // employee roles
+                    rolePrefix + clientRole + ", " + rolePrefix + userRole); // client roles
 
+        // retrieve auth data from database:
         auth.jdbcAuthentication()
                 .dataSource(dataSource)
-                .withDefaultSchema()
-                .usersByUsernameQuery(
-                        "select email,\"password\",true "
-                                + "from accounts ac join \"admin\" ad on ac.user_id = ad.id"
-                                + "where email = ?"
-                )
-                .authoritiesByUsernameQuery(
-                        "select email,ROLE_ADMIN "
-                                + "from accounts ac join \"admin\" ad on ac.user_id = ad.id"
-                                + "where email = ?"
-                );
+                .passwordEncoder(NoOpPasswordEncoder.getInstance()) // TODO change to REAL password encoder, e.g. BCrypt
+                .usersByUsernameQuery("""
+                        select email,"password",true
+                        from accounts ac
+                        where email = ?
+                        """
+                ) // query for retrieving list of accounts
+                .authoritiesByUsernameQuery(authoritiesQuery); // query for retrieving list of roles of these accounts
     }
 
+    /**
+     * Configuration of http requests permissions: assigning URIs to roles, defining login page and redirects.
+     * @param http http requests config
+     * @throws Exception when configuration fails
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers(adminUri).hasRole(adminRole)
                 .antMatchers(employeeUri).hasRole(employeeRole)
                 .antMatchers(clientUri).hasRole(clientRole)
+                .antMatchers(registerUri).permitAll()
                 .antMatchers(apiUserUri).hasRole(userRole)
                 .anyRequest().permitAll()
                 .and()
